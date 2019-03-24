2e20daa61fcaacea613991ae78bd5ee6fadadf1c
Robin Deits <robin.deits@gmail.com>
2019-03-24 12:36:16 -0400

    cleanup

--- src/solver.jl
+++ src/solver.jl
@@ -125,14 +125,6 @@ function _derive(rule::Rule,
     _derive(lhs(r), rhs(r), constituents, target)
 end
 
-
-# function derive!(state::SolverState, arc::Arc{Rule})
-#     get!(state.derivations, arc) do
-#         inputs = [solve!(state, c) for c in constituents(arc)]
-#         _derive(rule(arc), inputs)
-#     end
-# end
-
 function find_derivations!(state::SolverState, arc::Arc{Rule}, target::AbstractString)
     arc_derivations = get!(state.derivations, arc) do
         Dict()
@@ -170,79 +162,6 @@ function Base.show(io::IO, arc::DerivedArc)
     print(io, "; $(score(arc.arc))) -> \"$(arc.output)\")")
 end
 
-# struct CompletedArc
-#     head::GrammaticalSymbol
-#     constituents::NTuple{N, CompletedArc} where N
-#     output::String
-# end
-
-# function Base.show(io::IO, arc::CompletedArc)
-#     expand(io, arc, 0)
-# end
-
-# function expand(io::IO, arc::CompletedArc, indentation=0)
-#     print(io, "(", name(arc.head))
-#     arguments = arc.constituents
-#     for i in eachindex(arguments)
-#         if length(arguments) > 1
-#             print(io, "\n", repeat(" ", indentation + 2))
-#         else
-#             print(io, " ")
-#         end
-#         constituent = arguments[i]
-#         expand(io, constituent, indentation + 2)
-#     end
-#     print(io, " -> ", arc.output)
-#     print(io, ")")
-# end
-
-# @generated function (_completed_arcs(head::GrammaticalSymbol, constituents::Vector{PassiveArc},
-#                          outputs::Vector{String},
-#                          derivations::Vector{NTuple{N, String}},
-#                          target::Union{AbstractString, Nothing}=nothing)::Vector{CompletedArc}) where {N}
-#     quote
-#         if typeof(head) === Token
-#             return [CompletedArc(head, (), outputs[])]
-#         end
-#         results = CompletedArc[]
-#         for (output, inputs) in zip(outputs, derivations)
-#             # @show output inputs
-#             if target !== nothing && output != target
-#                 continue
-#             end
-#             # @show $(Expr(:tuple, [:(completed_arcs(constituents[$i], inputs[$i])) for i in 1:N]...))
-#             for completed_constituents in $(Expr(:call, :product, [:(completed_arcs(constituents[$i], inputs[$i])) for i in 1:N]...))
-#                 # @show completed_constituents
-#                 push!(results, CompletedArc(head, completed_constituents, output))
-#             end
-#         end
-#         results
-#     end
-# end
-
-# function completed_arcs(arc::PassiveArc, target::Union{AbstractString, Nothing}=nothing)::Vector{CompletedArc}
-#     # results = CompletedArc[]
-#     # for (output, inputs) in zip(arc.outputs, arc.derivations)
-#     #     if target !== nothing && output != target
-#     #         continue
-#     #     end
-#     #     for constituents in product(completed_arcs.(arc.constituents, inputs)...)
-#     #         push!(results, CompletedArc(lhs(rule(arc)), collect(constituents), output))
-#     #     end
-#     # end
-#     # results
-#     _completed_arcs(lhs(rule(arc)), constituents(arc), outputs(arc), arc.derivations, target)
-# end
-
-# function completed_arcs(chart::Chart)
-#     parses = complete_parses(chart)
-#     results = CompletedArc[]
-#     for passive_arc in parses
-#         append!(results, completed_arcs(passive_arc,))
-#     end
-#     results
-# end
-
 function answer_similarity(word1::AbstractString, word2::AbstractString)
     if word2 in keys(CACHE[].synonyms) && word1 in CACHE[].synonyms[word2]
         1.0
@@ -267,28 +186,3 @@ end
 definition(arc::SolvedArc) = definition(arc.arc)
 
 num_letters(word::AbstractString) = count(!isequal(' '), word)
-
-# function solutions(chart::Chart, len::Integer, pattern::Regex)
-#     results = Tuple{PassiveArc, String, Float64}[]
-#     for p in complete_parses(chart)
-#         for output in p.outputs
-#             if is_word(output) && num_letters(output) == len && occursin(pattern, output)
-#                 push!(results, (p, output, solution_quality(p, output)))
-#             end
-#         end
-#     end
-#     sort!(results, by = x -> x[3], rev = true)
-#     # results = [(p, solution_quality(p)) for p in complete_parses(chart)]
-#     # sort!(results, by=x -> x[2], rev=true)
-#     results
-# end
-
-# function solve(clue::AbstractString, len::Integer, pattern::Regex=r"")
-#     rules = cryptics_rules()
-#     grammar = Grammar(rules)
-#     tokens = normalize.(split(clue))
-#     filter!(!isempty, tokens)
-#     chart = chart_parse(tokens, grammar, TopDown());
-#     results = solutions(chart, len, pattern)
-#     results
-# end

80e3f03ef2493c04076d8aed8de9bbe7917e803f
Robin Deits <robin.deits@gmail.com>
2019-07-20 20:26:31 -0400

    auto accept

--- deps/build.jl
+++ deps/build.jl
@@ -1,5 +1,14 @@
-# using DataDeps
+using DataDeps
 
-# include("data_registration.jl")
-# datadep"OpenOffice-MyThes-1.0"
-# datadep"SCOWL-wordlist-en_US-large"
+# The license issues are covered in the Readme
+prev = get(ENV, "DATADEPS_ALWAYS_ACCEPT", nothing)
+try
+    ENV["DATADEPS_ALWAYS_ACCEPT"] = "true"
+    include("data_registration.jl")
+    datadep"OpenOffice-MyThes-1.0"
+    datadep"SCOWL-wordlist-en_US-large"
+finally
+    if prev !== nothing
+        ENV["DATADEPS_ALWAYS_ACCEPT"] = prev
+    end
+end

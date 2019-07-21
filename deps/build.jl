368f3bd6068d5135836cb16d1161a7516b738581
Robin Deits <robin.deits@gmail.com>
2019-07-20 20:34:08 -0400

    also wordnet

--- deps/build.jl
+++ deps/build.jl
@@ -5,6 +5,7 @@ prev = get(ENV, "DATADEPS_ALWAYS_ACCEPT", nothing)
 try
     ENV["DATADEPS_ALWAYS_ACCEPT"] = "true"
     include("data_registration.jl")
+    datadep"WordNet 3.0"
     datadep"OpenOffice-MyThes-1.0"
     datadep"SCOWL-wordlist-en_US-large"
 finally

dfb4139a8c2a9bd2776dd38500d26f8e201734f8
Robin Deits <robin.deits@gmail.com>
2019-07-21 20:44:48 -0400

    more license info

--- corpora/README.md
+++
@@ -1,2 +0,0 @@
-http://www.danielnaber.de/wn2ooo/
-https://www.openoffice.org/lingucomponent/thesaurus.html
--- deps/build.jl
+++ deps/build.jl
@@ -1,6 +1,8 @@
 using DataDeps
 using WordNet
 
+@warn "The build process for CrypticCrosswords.jl will automatically download the WordNet 3.0, OpenOffice-MyThes, and SCOWL-wordlist-en_US-large corpora. Please see https://github.com/rdeits/CrypticCrosswords.jl#additional-license-information for information about the licenses for these corpora."
+
 # The license issues are covered in the Readme
 prev = get(ENV, "DATADEPS_ALWAYS_ACCEPT", nothing)
 try
